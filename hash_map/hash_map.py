51356580061783c4eb0d69fbe27962699c1213b9
vasechkin <vasechkin@gmail.com>
2025-08-31 00:42:13 +0300

    feat: коллизия и ремув

--- hash_map/hash_map.py
+++ hash_map/hash_map.py
@@ -1,4 +1,11 @@
 class HashMap:
+    class Node:
+        def __init__(self, key, value, count = 1):
+            self.count = count
+            self.key = key
+            self.value = value
+            
+
     def __init__(self):
         self.__count = 0
         self.__size = 100
@@ -12,17 +19,34 @@ class HashMap:
         :param key: ключ
         """
         hash = self.__hash(key)
+        self.__count += 1
 
         if self.__memory[hash] is None:
-            self.__memory[hash] = value
+            self.__memory[hash] = HashMap.Node(key, value)
             return
         else:
-            counter = 1
-            while self.__memory[hash]: pass
+            i = 1
+            while self.__memory[hash] is not None:
+                hash = (hash + i**2) % self.__size
+                i += 1
             
-            
+            self.__memory[hash] = HashMap.Node(key, value)
+
+    def remove(self, key: any):
+        """
+        удалить элемент по ключу
+        """
+        for i in range(self.__size):
+            if self.__memory[i] is not None:
+                if self.__memory[i].key == key:
+                    self.__memory[i] = None
+                    self.__count -= 1
+                    return 
 
     def get(self, key) -> any:
+        """
+        найти значение по ключу
+        """
         hash = self.__hash(key)
         value = self.__memory[hash]
 
@@ -37,21 +61,3 @@ class HashMap:
 
     def __hash(self, key) -> int:
         return hash(key) % self.__size
-
-
-map = HashMap()
-
-for i in range(0, 600, 1):
-    print(f"Add:: key[{str(i)}]; value={i}")
-    map.add(str(i), i)
-
-print(HashMap.counter)
-
-
-collection = {
-    "пн": "mn",
-    "вт": "tu"
-}
-
-collection["ср"] = "wn"
-collection["вт"] = "tu2"
